f3c378e49f987407c9084671d18949545f35880e
p_castro <paul.crmst@gmail.com>
2019-11-07 19:40:41 +0200

    Add JWT Solution

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,14 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@authenticate');
+Route::get('open', 'DataController@open');
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('user', 'UserController@getAuthenticatedUser');
+    Route::get('closed', 'DataController@closed');
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
